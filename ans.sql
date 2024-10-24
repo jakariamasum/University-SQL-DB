48ae2169c7a67470b317c1b37c3fe43c2c9df77a
jakariamasum <jmphy698@gmail.com>
2024-10-24 12:18:30 +0600

    feat: student table created

---
+++ ans.sql
@@ -0,0 +1,11 @@
+-- Active: 1729750512256@@127.0.0.1@5432@university_db
+-- Create the students table
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    student_name VARCHAR(100) NOT NULL,
+    age INTEGER NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    frontend_mark INTEGER NOT NULL,
+    backend_mark INTEGER NOT NULL,
+    status VARCHAR(20)
+);
